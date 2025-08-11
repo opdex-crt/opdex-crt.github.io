210ef85bb2450838bc2fef8460e74aaeb94b271b
ChrysalisRoomTechnology <chryrt-co@outlook.com>
2025-08-11 14:19:43 +0800

    Update home.js

--- home.js
+++ home.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     '100704': '<h2>Research Report</h2><p>ID: 100704</p><p>Date: June 20</p><h3>Content Section</h3><p>Experimental Process:<br>A controlled experiment compared planar substrates (Group A) with nano-cone array substrates (Group B, tip curvature radius ≈200nm). Both groups used silicon wafers (99.999% purity) under identical OPDEX fields (50 kJ/s, Quantrix injector). Energy distribution was monitored via a Stellaron Field Mapper, with a 12-time-unit duration.</p><h3>Experimental Results:</h3><p>Adhesion Bias:<br>Group A (Planar): Surface adhesion coefficient = 0.07 (±0.02)<br>Group B (Tips): Adhesion coefficient at tips = 0.85±0.03, with energy density peaking at cone apices (Fig.1).</p><p>Stability Difference:<br>Group A energy retention time ≤5 sec;<br>Group B tip retention time ≥120 sec without significant decay (p<0.01).</p><p>Structural Integrity:<br>Group B cones showed zero fractures (density surge +15% only), while Group A developed macro-cracks (consistent with Report 100123).</p><h3>Experimental Conclusion:</h3><p>OPDEX energy exhibits significant adhesion preference for high-curvature tip structures, with stability attributed to Field-Induced Focusing. Tip-based designs mitigate density surge risks, providing an optimization path for solid-state energy receivers (e.g., OPDEX rectennas). Biological compatibility verification is required before advancing to in vivo trials.</p><h3>Notes Section:</h3><p>Stellaron Field Mapper: A 3D energy field mapping device.</p>'
   };
   
-  var g = document.getElementById('d');
+  var g = document.getElementById('d-content');
   var h = document.querySelectorAll('.c');
   
   h.forEach(function(i) {
